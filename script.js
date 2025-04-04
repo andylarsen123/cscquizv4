23fcc6850aed675ca5ac2768c136ca7e243a2136
andylarsen123 <andyr.larsen@gmail.com>
2025-04-03 21:02:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -240,8 +240,8 @@ function showQuestions(screenIndex) {
     heading.textContent = "Check all that apply (or leave blank if unsure):";
     heading.style.textAlign = 'left';
     heading.style.fontWeight = 'bold';
-    heading.style.marginBottom = '10px';
-    heading.style.margin = '10px';
+    heading.style.marginBottom = '5px';
+    heading.style.margin = '13px';
     form.appendChild(heading);
 
     // Create questions for this screen
